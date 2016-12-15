bc10dfee5e00c2531af8270756033583ebcf1cbe
Chawasit Tengtrairatana <chawasit_teng@cmu.ac.th>
2016-12-15 18:57:25 +0700

    update
    + stable userBuffer
    + round count from 1 - 4
    + show all zero when reset is H

--- userbuffer.v
+++ userbuffer.v
@@ -3,33 +3,42 @@ module UserBuffer(s0, s1, press, a ,b, c ,d, reset);
 	output s0, s1, press;
 	reg s0, s1, press;
 	wire pressin;
-
+	reg pressing;
+	or orAll(pressin, !a, !b, !c, !d, reset);
 	always
 		begin
-			if(~a) begin
-				s1 <= 1'b0;
-				s0 <= 1'b0;
-				press <= 1'b1;
-			end
-			else if(~b) begin
-				s1 <= 1'b0;
-				s0 <= 1'b1;
-				press <= 1'b1;
-			end
-			else if(~c) begin
-				s1 <= 1'b1;
-				s0 <= 1'b0;
-				press <= 1'b1;
-			end
-			else if(~d) begin
-				s1 <= 1'b1;
-				s0 <= 1'b1;
-				press <= 1'b1;
-			end
-			else if(reset) begin
-				s0 <= 1'b0;
-				s1 <= 1'b0;
-				press <= 1'b0;
-			end
+		// pressin <= (~a) || (~b) || (~c) || (~d) || reset;
+		if(reset) begin
+			s0 <= 1'b0;
+			s1 <= 1'b0;
+			press <= 1'b0;
 		end
+		else if(pressin) begin
+			if(~pressing) begin
+				pressing <= 1'b1;
+				if(~a) begin
+					s1 <= 1'b0;
+					s0 <= 1'b0;
+					press <= 1'b1;
+				end
+				else if(~b) begin
+					s1 <= 1'b0;
+					s0 <= 1'b1;
+					press <= 1'b1;
+				end
+				else if(~c) begin
+					s1 <= 1'b1;
+					s0 <= 1'b0;
+					press <= 1'b1;
+				end
+					else if(~d) begin
+					s1 <= 1'b1;
+					s0 <= 1'b1;
+					press <= 1'b1;
+				end
+							end
+		end else begin
+			pressing <= 1'b0;
+		end
+	end
 endmodule
